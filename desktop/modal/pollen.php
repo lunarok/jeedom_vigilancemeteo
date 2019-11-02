05141e1b81c209aa63977b4137bd08f00c99c79b
DUPONT <cedric.dupont@atos.net>
2019-11-02 20:58:24 +0100

    evol template pollen

--- desktop/modal/pollen.php
+++ desktop/modal/pollen.php
@@ -20,16 +20,155 @@ if (init('id') == '') {
     throw new Exception('{{L\'id de l\'équipement ne peut etre vide : }}' . init('op_id'));
 }
 
+?>
+
+<style>
+	#pollen_carousel#id# {
+	  position: relative;
+	  width: 186px;
+	  margin: 0 auto;
+	  display: table-cell;
+	  top: -12px;
+	}
+	#pollen_slides#id# {
+	  overflow: hidden;
+	  position: relative;
+	  width: 186px;
+	  height: 76px;
+	}
+	#pollen_slides#id# ul {
+	  list-style: none;
+	  width: 186px;
+	  height: 76px;
+	  margin: 0;
+	  padding: 0;
+	  position: relative;
+	}
+	#pollen_slides#id# li {
+	  width: 186px;
+	  height: 76px;
+	  float: left;
+	  text-align: center;
+	  position: relative;
+	}
+	.pollen_btn-bar#id# {
+	  position: relative;
+	  position: relative;
+	  display: table-cell;
+	  height: 76px;
+	  top: -11px;
+	}
+	.pollen_button_L#id# {
+	  padding: 0 8px 0 6px;
+	  display: table-cell;
+	}
+	.pollen_button_R#id# {
+	  padding: 0 6px 0 8px;
+	  display: table-cell;
+	}
+	#pollen_buttons#id# a {
+	  vertical-align: middle;
+	  float: left;
+	  outline: 0;
+	  text-decoration: none;
+	  width: 15px;
+	  height: 76px;
+	  padding-top: 28px;
+	  opacity: 0.8;
+	}
+	#pollen_prev#id# {
+	  border-right: solid;
+	  border-right-width: 1px;
+	  border-right-color: gray;
+	}
+	#pollen_next#id# {
+	  border-left: solid;
+	  border-left-width: 1px;
+	  border-left-color: gray;
+	}
+	a#pollen_prev#id#:hover,
+	a#pollen_next#id#:hover {
+	  border-right-color: white;
+	  border-left-color: white;
+	}
+	.pollen_quote-phrase#id# {
+	  text-align: left;
+	  display: table-cell;
+	  height: 76px;
+	  font-size: 12px;
+	  color: #FFF;
+	  text-shadow: .5px 0px #b14943;
+	}
+	.pollen_quoteContainer#id# {
+	  display: table;
+	  width: 100%;
+	}
+	.pollen_pollen#id# {
+	  padding: 1px 0 1px;
+	}
+	.pollen_label#id# {
+	  display: table-cell;
+	  font-size: 11px;
+	  width: 74px;
+	  height: 16px;
+	}
+	.pollen_value#id# {
+	  display: table-cell;
+	  width: 15px;
+	  text-align: center;
+	  font-style: normal;
+	}
+	.pollen_graph#id# {
+	  display: table-cell;
+	  height: 16px;
+	}
+	.pollen_general#id# {
+	  display: table-cell;
+	  vertical-align: middle;
+	  align: center;
+	  cursor:default;
+	  font-size: 1.5em;
+	  font-weight: bold;
+	  border-style: solid;
+	  border-width: 1px;
+	  border-color: #ffffff;
+	  border-radius:19px;
+	  width:38px;
+	  height:38px;
+	}
+</style>
+
+<?php
+
 $id = init('id');
 $eqLogic = vigilancemeteo::byId($id);
+$onetemplate = getTemplate('core', $version, '1pollen', 'vigilancemeteo');
+$replace = $this->preToHtml($_version);
 
 foreach ($eqLogic->getCmd('info') as $cmd) {
+  switch ($cmd->execCmd()) {
+    case '-1': $color = 'black'; break;
+    case '0':  $color = 'black'; break;
+    case '1':  $color = 'lime';  break;
+    case '2':  $color = 'green'; break;
+    case '3':  $color = 'yellow'; break;
+    case '4':  $color = 'orange'; break;
+    case '5':  $color = 'red';    break;
+  }
   if ($cmd->getLogicalId() != 'general') {
     $sort[$cmd->getLogicalId()] = $cmd->execCmd();
+    $unitreplace['#id#'] = $cmd->getId();
+    $unitreplace['#value#'] = $cmd->execCmd();
+    $unitreplace['#name#'] = $cmd->getName();
+    $unitreplace['#width#'] = $cmd->execCmd() * 20;
+    $unitreplace['#color#'] = $color;
+    $unitreplace['#background-color#'] = $replace['#background-color#'];
+    $slide[$cmd->getLogicalId()] = template_replace($unitreplace, $onetemplate);
   }
 }
 arsort($sort);
 foreach ($sort as $key => $value) {
-  echo $key . ' : ' . $value . '<br>';
+  echo $slide[$key];
 }
+
 ?>
